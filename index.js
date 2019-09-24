e77cfe083da1a4b3476937db3fb9b0d7a1e3d34b
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-24 15:30:59 -0600

    submit and reset button listeners work

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 'use strict';
+
+const STORE = [
+]
+
+function calculateSubtotal(){
+
+}
+
+function calculateTip(){
+
+}
+
+function calculateTotal(){
+
+}
+
+// function determineWhichButtonWasClicked(){
+//   $('main').on('submit', '.enterTheMealDetailsCalculator', function(event){
+//     event.preventDefault();
+//     if ($(event.target).hasClass('submitCalc')){
+//     submitButtonPressed();
+//     } else {
+//       resetMealDetailsButtonPressed();
+//     }
+//   }
+//   )
+//   }
+
+
+function submitButtonPressed(){
+$('.submitButton').submit(function(event){
+   console.log('submit button!')
+})
+}
+
+function resetMealDetailsButtonPressed(){
+  $('.resetCalcButton').submit(function(event){
+  console.log('reset button!');
+  })
+  
+}
+
+function calculateTipTotal(){
+
+}
+
+function updateMealCount(){
+
+}
+
+function updateAverageTipPerMeal(){
+
+}
+
+function resetEntireCalculator(){
+
+}
+
+function renderPage(){
+ submitButtonPressed();
+  resetMealDetailsButtonPressed();
+  //determineWhichButtonWasClicked();
+  resetEntireCalculator();
+}
+
+$(renderPage);
