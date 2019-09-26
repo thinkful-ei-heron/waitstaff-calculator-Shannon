2bea41f776807a5ca4eef389a5a858eed3f87860
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-25 22:55:07 -0600

    final edit

--- index.js
+++ index.js
@@ -27,49 +27,31 @@ function getNewestObj() {
 }
 function calculateSubtotal(obj) {
   let base = obj.baseMealPrice;
-  //console.log(base);
   let tax = obj.taxRate;
-  //console.log(tax);
   let result = base * tax + base;
-  //console.log(result);
   let roundedResultOne = Math.ceil(result * 100) / 100;
-  //console.log(roundedResultOne);
   let roundedResult = roundedResultOne.toFixed(2);
-  //console.log(roundedResult);
   obj.subTotal = parseFloat(roundedResult);
-  //console.log(obj.subTotal);
   $('.updateSubtotal').html(`$${obj.subTotal}`);
 }
 
 function calculateTip(obj) {
   let tipPercent = obj.tipPercentage;
-  //console.log(tipPercent);
   let subTotal = obj.subTotal;
-  //console.log(subTotal);
   let tipAmount = tipPercent * subTotal;
-  //console.log(tipAmount);
   let roundedTipAmountOne = Math.ceil(tipAmount * 100) / 100;
-  //console.log(roundedTipAmountOne);
   let roundedTipAmount = roundedTipAmountOne.toFixed(2);
-  //console.log(roundedTipAmount);
   obj.tip = parseFloat(roundedTipAmount);
-  //console.log(obj.tip);
   $('.updateTip').html(`$${obj.tip}`);
 }
 
 function calculateTotal(obj) {
   let tip = obj.tip;
-  //console.log(tip);
   let subTotal = obj.subTotal;
-  //console.log(subTotal);
   let total = tip + subTotal;
-  //console.log(total);
   let roundedTotal = Math.ceil(total * 100) / 100;
-  //console.log(roundedTotal);
   let twoDecimalTotal = roundedTotal.toFixed(2);
-  //console.log(twoDecimalTotal);
   obj.total = parseFloat(twoDecimalTotal);
-  //console.log(obj.total);
   $('.updateTotal').html(`$${obj.total}`);
 
 }
@@ -95,18 +77,6 @@ function submitButtonPressed() {
   });
 }
 
-// I was able to comment out the following because 
-//The <input type="reset"> button defines a reset button 
-//which resets all form values to its initial values.
-
-// function resetMealDetailsButtonPressed() {
-//   $('.enterTheMealDetailsCalculator').on('reset', '.resetCalcButton', function(event) {
-//     event.preventDefault();
-//     console.log('reset button clicked');
-//     $('input').val('');
-//   });
-// }
-
 function updateMyEarnings(obj) {
   calculateTipTotal(obj);
   updateMealCount();
@@ -119,13 +89,9 @@ function calculateTipTotal(obj) {
     myEarnings.tipTotal = (0.00).toFixed(2);
   } else {
     let currentDailyTips = myEarnings.tipTotal;
-    //console.log(currentDailyTips);
     let incomingTip = obj.tip;
-    //console.log(incomingTip);
     let updatedTip = (currentDailyTips + incomingTip).toFixed(2);
-    //console.log(updatedTip);
     myEarnings.tipTotal = parseFloat(updatedTip);
-    //console.log(myEarnings.tipTotal);
   }
   $('.updateTipTotal').html(`$${myEarnings.tipTotal}`);
 
@@ -167,7 +133,6 @@ function resetEntireCalculator() {
 
 function renderPage() {
   submitButtonPressed();
-  //resetMealDetailsButtonPressed();
   resetEntireCalculator();
 }
 
