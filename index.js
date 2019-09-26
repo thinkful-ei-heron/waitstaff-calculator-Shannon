cb332d48bc3dd1c49d33b1f8e07e0738ba54e187
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-25 20:21:44 -0600

    improving STORE readability

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 
 const STORE = {
   todaysCustomers: [
-    { baseMealPrice: 0.00, taxRate: 0.00, tipPercentage: 0.00, subTotal: 0.00, tip: 0.00, total: 0.00 }
+    { 
+      baseMealPrice: 0.00,
+      taxRate: 0.00,
+      tipPercentage: 0.00,
+      subTotal: 0.00,
+      tip: 0.00,
+      total: 0.00 
+    }
   ],
   myEarnings:
-    { tipTotal: 0.00, mealCount: 0, averageTipPerMeal: 0.00 }
+    { 
+      tipTotal: 0.00,
+      mealCount: 0,
+      averageTipPerMeal: 0.00
+    }
 
 };
 
