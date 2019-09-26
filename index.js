05b5e99124b8b155daacc62dbe18caa7b77746ac
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-25 22:45:04 -0600

    fixed parseFloat functionality

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-'use strict';
+
 
 const STORE = {
   todaysCustomers: [
-    { 
+    {
       baseMealPrice: 0.00,
       taxRate: 0.00,
       tipPercentage: 0.00,
       subTotal: 0.00,
       tip: 0.00,
-      total: 0.00 
+      total: 0.00
     }
   ],
   myEarnings:
-    { 
-      tipTotal: 0.00,
-      mealCount: 0,
-      averageTipPerMeal: 0.00
-    }
+  {
+    tipTotal: 0.00,
+    mealCount: 0,
+    averageTipPerMeal: 0.00
+  }
 
 };
 
@@ -27,31 +27,49 @@ function getNewestObj() {
 }
 function calculateSubtotal(obj) {
   let base = obj.baseMealPrice;
-  let tax= obj.taxRate;
-  let result= (parseFloat(base) * parseFloat(tax)) + parseFloat(base);
-  let roundedResultone= Math.ceil(result * 100) / 100;
-  let roundedResult = roundedResultone.toFixed(2);
-  obj.subTotal= roundedResult;
+  //console.log(base);
+  let tax = obj.taxRate;
+  //console.log(tax);
+  let result = base * tax + base;
+  //console.log(result);
+  let roundedResultOne = Math.ceil(result * 100) / 100;
+  //console.log(roundedResultOne);
+  let roundedResult = roundedResultOne.toFixed(2);
+  //console.log(roundedResult);
+  obj.subTotal = parseFloat(roundedResult);
+  //console.log(obj.subTotal);
   $('.updateSubtotal').html(`$${obj.subTotal}`);
 }
 
 function calculateTip(obj) {
   let tipPercent = obj.tipPercentage;
-  let subTotal= obj.subTotal;
-  let tipAmount= tipPercent * subTotal;
-  let roundedTipAmountone = Math.ceil(tipAmount * 100) / 100;
-  let roundedTipAmount = roundedTipAmountone.toFixed(2);
-  obj.tip = roundedTipAmount;
+  //console.log(tipPercent);
+  let subTotal = obj.subTotal;
+  //console.log(subTotal);
+  let tipAmount = tipPercent * subTotal;
+  //console.log(tipAmount);
+  let roundedTipAmountOne = Math.ceil(tipAmount * 100) / 100;
+  //console.log(roundedTipAmountOne);
+  let roundedTipAmount = roundedTipAmountOne.toFixed(2);
+  //console.log(roundedTipAmount);
+  obj.tip = parseFloat(roundedTipAmount);
+  //console.log(obj.tip);
   $('.updateTip').html(`$${obj.tip}`);
 }
 
 function calculateTotal(obj) {
-  let tip= obj.tip;
-  let subTotal= obj.subTotal;
-  let total= parseFloat(tip) + parseFloat(subTotal);
-  let roundedTotal= Math.ceil(total * 100) / 100;
-  let twoDecimalTotal= roundedTotal.toFixed(2);
-  obj.total = twoDecimalTotal;
+  let tip = obj.tip;
+  //console.log(tip);
+  let subTotal = obj.subTotal;
+  //console.log(subTotal);
+  let total = tip + subTotal;
+  //console.log(total);
+  let roundedTotal = Math.ceil(total * 100) / 100;
+  //console.log(roundedTotal);
+  let twoDecimalTotal = roundedTotal.toFixed(2);
+  //console.log(twoDecimalTotal);
+  obj.total = parseFloat(twoDecimalTotal);
+  //console.log(obj.total);
   $('.updateTotal').html(`$${obj.total}`);
 
 }
@@ -67,13 +85,13 @@ function updateCustomerCharges() {
 function submitButtonPressed() {
   $('.enterTheMealDetailsCalculator').submit(function (event) {
     event.preventDefault();
-    if ($('#baseMealPrice').val() === '' || $('#taxRate').val() === '' || $('#tipPercentage').val()=== '') {
-      alert('You must fill in all entry fields');
-    } else {
-    STORE.todaysCustomers.push({ baseMealPrice: $('#baseMealPrice').val(), taxRate: ($('#taxRate').val() * 0.01), tipPercentage: ($('#tipPercentage').val()*0.01)});
+    STORE.todaysCustomers.push({
+      baseMealPrice: parseFloat($('#baseMealPrice').val()),
+      taxRate: parseFloat(($('#taxRate').val() * 0.01)),
+      tipPercentage: parseFloat(($('#tipPercentage').val() * 0.01)) 
+    });
     $('input').val('');
     updateCustomerCharges();
-    }
   });
 }
 
@@ -97,13 +115,17 @@ function updateMyEarnings(obj) {
 
 function calculateTipTotal(obj) {
   let myEarnings = STORE.myEarnings;
-  if(STORE.todaysCustomers.length === 1 ){
+  if (STORE.todaysCustomers.length === 1) {
     myEarnings.tipTotal = (0.00).toFixed(2);
   } else {
-    let currentDailyTips= myEarnings.tipTotal;
+    let currentDailyTips = myEarnings.tipTotal;
+    //console.log(currentDailyTips);
     let incomingTip = obj.tip;
-    let updatedTip = (parseFloat(currentDailyTips) + parseFloat(incomingTip)).toFixed(2);
-    myEarnings.tipTotal = updatedTip;
+    //console.log(incomingTip);
+    let updatedTip = (currentDailyTips + incomingTip).toFixed(2);
+    //console.log(updatedTip);
+    myEarnings.tipTotal = parseFloat(updatedTip);
+    //console.log(myEarnings.tipTotal);
   }
   $('.updateTipTotal').html(`$${myEarnings.tipTotal}`);
 
@@ -112,11 +134,11 @@ function calculateTipTotal(obj) {
 
 function updateMealCount() {
   let myEarnings = STORE.myEarnings;
-  if(STORE.todaysCustomers.length === 1) {
+  if (STORE.todaysCustomers.length === 1) {
     myEarnings.mealCount = 0;
   } else {
     let currentMealCount = myEarnings.mealCount;
-    currentMealCount+= 1;
+    currentMealCount += 1;
     myEarnings.mealCount = currentMealCount;
   }
   $('.updateMealCount').html(`${myEarnings.mealCount}`);
@@ -124,12 +146,12 @@ function updateMealCount() {
 
 function updateAverageTipPerMeal() {
   let myEarnings = STORE.myEarnings;
-  if(STORE.todaysCustomers.length === 1){
+  if (STORE.todaysCustomers.length === 1) {
     myEarnings.averageTipPerMeal = (0.00).toFixed(2);
   } else {
     let tips = myEarnings.tipTotal;
-    let numMeals= myEarnings.mealCount;
-    let average = (tips/numMeals).toFixed(2);
+    let numMeals = myEarnings.mealCount;
+    let average = (tips / numMeals).toFixed(2);
     myEarnings.averageTipPerMeal = average;
   }
   $('.averageTipPerMeal').html(`$${myEarnings.averageTipPerMeal}`);
@@ -137,7 +159,7 @@ function updateAverageTipPerMeal() {
 
 function resetEntireCalculator() {
   $('.resetEntireApp').submit(function (event) {
-    STORE.todaysCustomers = STORE.todaysCustomers.slice(0,1);
+    STORE.todaysCustomers = STORE.todaysCustomers.slice(0, 1);
     updateCustomerCharges();
   });
 
